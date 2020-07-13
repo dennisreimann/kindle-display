2bbbf77b470a28f73e9ce2339937850d2c5511b8
Christoph Sperle <christoph@sperle.de>
2020-07-13 10:38:40 +0200

    only show battery level if using battery

--- kindle/mnt/base-us/update.sh
+++ kindle/mnt/base-us/update.sh
@@ -5,7 +5,6 @@ FILE=display.png
 
 savePower=false
 batteryLevel=$(lipc-get-prop com.lab126.powerd battLevel)
-batteryBar=$(($batteryLevel/10))
 now=$(date +'%F %R')
 
 cd "$(dirname "$0")"
@@ -28,19 +27,21 @@ fi
 eips 0 0 "$now"
 
 # display battery level
-case "$batteryBar" in
-  0)  eips 38 0 "|-----$batteryLevel--->|" ;;
-  1)  eips 38 0 "|----$batteryLevel-->=|" ;;
-  2)  eips 38 0 "|----$batteryLevel->==|" ;;
-  3)  eips 38 0 "|----$batteryLevel>===|" ;;
-  4)  eips 38 0 "|----$batteryLevel====|" ;;
-  5)  eips 38 0 "|----$batteryLevel====|" ;;
-  6)  eips 38 0 "|--->$batteryLevel====|" ;;
-  7)  eips 38 0 "|-->=$batteryLevel====|" ;;
-  8)  eips 38 0 "|->==$batteryLevel====|" ;;
-  9)  eips 38 0 "|>===$batteryLevel====|" ;;
-  10)  eips 38 0 "|====100===|" ;;
-esac
+if [ $batteryLevel -lt 100 ]; then
+  batteryBar=$(($batteryLevel/10))
+  case "$batteryBar" in
+    0)  eips 38 0 "|-----$batteryLevel--->|" ;;
+    1)  eips 38 0 "|----$batteryLevel-->=|" ;;
+    2)  eips 38 0 "|----$batteryLevel->==|" ;;
+    3)  eips 38 0 "|----$batteryLevel>===|" ;;
+    4)  eips 38 0 "|----$batteryLevel====|" ;;
+    5)  eips 38 0 "|----$batteryLevel====|" ;;
+    6)  eips 38 0 "|--->$batteryLevel====|" ;;
+    7)  eips 38 0 "|-->=$batteryLevel====|" ;;
+    8)  eips 38 0 "|->==$batteryLevel====|" ;;
+    9)  eips 38 0 "|>===$batteryLevel====|" ;;
+  esac
+fi
 
 if [ "$savePower" = true ]; then
   # power down networking
