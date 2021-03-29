e1b1cfbe2be442329ae9e598336b6707fca8ad74
Dennis Reimann <mail@dennisreimann.de>
2021-03-29 08:22:22 +0200

    Firefox compatibility on macOS

--- server/cron.sh
+++ server/cron.sh
@@ -12,5 +12,10 @@ cd $dir
 
 # create screenshot
 cd $dir/public
+
+if ! type firefox &> /dev/null && [[ -d "/Applications/Firefox.app/Contents/MacOS/" ]]; then
+  PATH="$PATH:/Applications/Firefox.app/Contents/MacOS"
+fi
+
 firefox --screenshot http://localhost:$DISPLAY_SERVER_PORT --window-size=600,800
 pngcrush -c 0 screenshot.png display.png
